af321ff04a87b0eb3188f8f07edac6bc5c45a638
Mirko Martinovic <bgmirko@hotmail.com>
2019-05-14 21:20:47 +0200

    Bug fix

--- JwtRedirect/index.js
+++ JwtRedirect/index.js
@@ -62,7 +62,7 @@ module.exports = async function (context, req) {
         } else {
             copyExamVersionResponse = testIfExamBlobAlreadyExist;
 
-            redirect = verifyTokenResponse.fe_endpoint + '/finish?status=false';
+            redirect = verifyTokenResponse.fe_endpoint + 'finish?status=false';
         }
 
         context.res = {
