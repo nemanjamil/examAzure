9a6e72af3f733e01072c446e2d4ecd5265cc8fff
Mirko Martinovic <bgmirko@hotmail.com>
2020-01-11 12:29:09 +0100

    Generate token with random uuid ExamEvent_External_id and configure send mail to be multilinguage

--- generateExamAndTokenPostman/index.js
+++ generateExamAndTokenPostman/index.js
@@ -2,32 +2,38 @@ const sgMail = require('@sendgrid/mail');
 const Exam = require('../models/exam');
 const secret_key = process.env.secret_key;
 const { connectionToDB } = require('../utils/database');
-const { responseErrorJson, responseOkJson } = require('../utils/common');
+const { responseErrorJson, responseOkJson, parseJsonArrayToKeyValue } = require('../utils/common');
+const { getSpecificDataFromDB } = require('../utils/database');
 const examtemplatecontainer = process.env.examtemplatecontainer;
 const UtilsBlob = require('../utils/utilsBlob');
 const examssk = process.env.EXAMSSK;
 const jwt = require('jsonwebtoken');
+const uuid =  require('uuid');
 
 module.exports = async function (context, req) {
 
-    const genarateExamData = req.body.data;
-    const participantFirstName = genarateExamData.Participant_Firstname;
+    const generateExamData = req.body.data;
+    const participantFirstName = generateExamData.Participant_Firstname;
     const participantEmail = req.body.participantEmail;
     const proctorEmailReceiver =  req.body.proctorEmailReceiver;
     const reactAppBaseUrl = req.body.reactAppBaseUrl;
+    const language = req.body.data.language;
 
     try {
         await connectionToDB();
 
         let response = {};
 
-        const generateTokenResult = await generateToken(genarateExamData);
+        generateExamData.ExamEvent_EXTERNAL_ID = uuid.v1();
+
+        const generateTokenResult = await generateToken(generateExamData);
 
         response = {
             generateTokenResult: generateTokenResult
         }
 
         let blobLocation = generateTokenResult.data.ExamVersion_EXTERNAL_ID+".json";
+
         let getJsonExamBlobResponse = await UtilsBlob.getJsonExamBlob(blobLocation, examtemplatecontainer);
 
         let getJsonExamBlobResponseParsed = JSON.parse(getJsonExamBlobResponse);
@@ -49,7 +55,7 @@ module.exports = async function (context, req) {
         }
                                         
         const proctorEmailReceiver =  req.body.proctorEmailReceiver;
-        const sendMailResponse = await sendMail(generateTokenResult.token, participantFirstName, participantEmail, proctorEmailReceiver, reactAppBaseUrl);
+        const sendMailResponse = await sendMail(generateTokenResult.token, participantFirstName, participantEmail, proctorEmailReceiver, reactAppBaseUrl, language);
 
         response = {
             ...response,
@@ -130,28 +136,41 @@ const saveExamInDB = async (generateTokenData, examId, ExamVersion_maxPoints,
     }
 }
 
-const sendMail = async (token, participantFirstName, participantEmail, proctorEmailReceiver, reactAppBaseUrl) => {
+const sendMail = async (token, participantFirstName, participantEmail, proctorEmailReceiver, reactAppBaseUrl, language) => {
+
+    let fieldsDB = ['NEW_EXAM_EMAIL_TITLE', 'NEW_EXAM_EMAIL_MESSAGE', 'STATUS_EMAIL_HI'];
+    const getDbDataForEmailTemplate = await getSpecificDataFromDB(fieldsDB);
+    let parseJsonArrayToKeyValueRes = await parseJsonArrayToKeyValue(getDbDataForEmailTemplate);
+
+    const EMAIL_TITLE = parseJsonArrayToKeyValueRes[fieldsDB[0]][language]; 
+    const EMAIL_MESSAGE = parseJsonArrayToKeyValueRes[fieldsDB[1]][language]; 
+    const STATUS_EMAIL_HI = parseJsonArrayToKeyValueRes[fieldsDB[2]][language]; 
+
+  //  console.log(EMAIL_MESSAGE);
+  //  console.log(STATUS_EMAIL_HI);
 
     const sendData = {};
-    sendData.title = 'New exam is created';
-    sendData.message = 'Mail is successfully sent';
+    sendData.title = EMAIL_TITLE;
+    sendData.message = EMAIL_MESSAGE;
     sendData.firstname = participantFirstName;
     sendData.participantemail = participantEmail;
     sendData.proctor_email_receiver = proctorEmailReceiver;
-    sendData.nameofsender = "TEMS";
+    sendData.nameofsender = "EXAM";
     sendData.token = `${reactAppBaseUrl}/JwtRedirect?token=${token}`;
 
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
-        to: sendData.participantemail,
-        bcc: [sendData.proctor_email_receiver],
+        to: sendData.proctor_email_receiver,
+        replayTo: sendData.participantemail,
         from: {
             name: sendData.nameofsender,
             email: process.env.SENDGRID_FROM_MAIL
         },
         templateId: process.env.TEMPLATE_ID_SENDGRID,
         dynamic_template_data: {
+            hello: STATUS_EMAIL_HI,
             subject: sendData.title,
+            status_sentence: sendData.message,
             name: sendData.firstname,
             linktoexam: sendData.token
         },
