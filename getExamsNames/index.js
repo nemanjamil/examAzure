c9dd9e7ac999f14cea12499b545bf82b67abd35a
nemanjamil <nemanjamil@gmail.com>
2020-03-25 12:14:24 +0100

    Add planned duration to exam object

--- getExamsNames/index.js
+++ getExamsNames/index.js
@@ -39,6 +39,10 @@ const getOneBlobJson = async (name) =>  {
     let blobLocation = name
     let responseFromBlob = await UtilsBlob.getJsonExamBlob(blobLocation, examtemplatecontainer);
     var obj = await JSON.parse(responseFromBlob); 
-    return { name : blobLocation, jsonExamName : obj.ExamVersion_Name, customName :  obj.ExamVersion_CustomName };
+
+    return { name : blobLocation, 
+             jsonExamName : obj.ExamVersion_Name, 
+             customName :  obj.ExamVersion_CustomName,
+             plannedDuration: obj.ExamVersion_plannedDuration };
 }
 
